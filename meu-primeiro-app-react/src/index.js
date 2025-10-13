f850153a24b5c2dc56b5630e09f63fbfe669b00f
Tiago Xavier <822148404@ulife.com.br>
2025-10-13 11:05:56 -0300

    feat: Create first react app: intro

--- meu-primeiro-app-react/src/index.js
+++ meu-primeiro-app-react/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import './styles.css';
 
 const App= () => {
-  return <div style={{margin:'auto', width: 768,backgroundColor:'#EEE', padding:12,borderRadius: 8}}>
+const estilosBotao = {marginTop:12, paddingTop: 8, paddingBottom:8, backgroundColor: 'blueviolet', color: 'white' ,border:'none', width: '100%', borderRadius: 8};
 
-<label for="nome" style={{display: 'block',marginBottom:4}}>Nome:</label>
-<input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8,
-borderStyle:'hidden', width:'100%', borderRadius: 8, outline:
-'none', boxSizing:'border-box'}}/>
+const textoRotulo = "Nome:";
 
-<button style={{marginTop: 12, paddingTop: 8, paddingBottom:8, backgroundColor: 'blueviolet', color: 'white' ,border:'none', width: '100%', borderRadius: 8,}}>Enviar</button>
-</div>}
+  return  (
+    <div style={{margin:'auto', width: 768,backgroundColor:'#EEE', padding:12,borderRadius: 8}}>
+
+  <label className="rotulo" htmlFor="nome" style={{display: 'block',marginBottom:4}}>{textoRotulo}</label>
+  <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8,
+  borderStyle:'hidden', width:'100%', borderRadius: 8, outline:
+  'none', boxSizing:'border-box'}}/>
+
+  <button style={estilosBotao}>Enviar</button>
+  </div>
+  )
+}
 
 const root = createRoot(document.getElementById('root'));  
 root.render(<App/>);
