43d27ccaa701d89736d9c7b2aa1a8b62a3d12829
Tiago Xavier <822148404@ulife.com.br>
2025-11-17 09:31:02 -0300

    Delete: mudança de branc

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const container = createRoot(document.getElementById('root'));
-container.render(<App/>)
-
